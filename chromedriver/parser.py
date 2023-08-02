c3ef2e4b8ef36dba133729e9c28c78a4467c9734
EgorFedotov <egorfedotovarz@gmail.com>
2023-08-02 16:04:58 +0300

    bug fix

--- chromedriver/parser.py
+++ chromedriver/parser.py
@@ -100,4 +100,3 @@ if __name__ == '__main__':
     except Exception as error:
         logger.error(error)
     
-    
---
+++ chromedriver/settings.py
@@ -0,0 +1,8 @@
+url = https://www.avito.ru/kartaly?cd=1&q=%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE
+chat_id = 
+tg_token = 
+num_ads = 1
+freq = 5
+keys = бенз
+max_price = 10000
+min_price = 1000
